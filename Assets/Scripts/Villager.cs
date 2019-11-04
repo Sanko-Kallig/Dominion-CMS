2b58d7fde5cd8ab12aa982a918685cae00825ad0
Sanko-Kallig <adk_m91@hotmail.com>
2019-11-04 20:10:30 +0100

    Fixed pathfinding with new structures, and added villager classes

---
+++ Assets/Scripts/Villager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Villager 
+{
+    public float Hitpoints;
+    public float Food;
+    public float Comfort;
+    public enum Job { Harvester, Woodcutter};
+    public Job VillagerJob;
+
+    public Villager(float hitpoints, float food, float comfort)
+    {
+        Hitpoints = hitpoints;
+        Food = food;
+        Comfort = comfort;
+    }
+}
--- Assets/Scripts/VillagerController.cs
+++ Assets/Scripts/VillagerController.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.AI;
 
 public class VillagerController : MonoBehaviour
 {
+    public Villager CurrentVillager;
+    public NavMeshAgent agent;
+    public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
+        CurrentVillager = new Villager(100, 100, 100);
+        CurrentVillager.VillagerJob = Villager.Job.Harvester;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
     }
 }
